7e9e30cde6798341c2cf4008ff2ab82c2075b53a
Joemer <joemers97@gmail.com>
2025-01-23 16:45:58 -0500

    added footer components

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,28 +1,33 @@
 import { Link } from 'react-router-dom';
 import './Landing.css';
+import '../../components/Footer/Footer';
 
 const Landing = () => {
 	return (
-		<main>
-			<img className='logo_page' src='/public/logo_page.png' alt='logo' />
+		<>
+			<main>
+				<img className='logo_page' src='/public/logo_page.png' alt='logo' />
 
-			<h1>
-				Welcome to FilmFav – Your Go-To Hub for Discovering and Sharing Movies!
-			</h1>
+				<h1>
+					Welcome to FilmFav – Your Go-To Hub for Discovering and Sharing
+					Movies!
+				</h1>
 
-			<h3>
-				Explore cinema with FilmFav! <span />
-				Create lists, discover trends, and connect with movie lovers.
-				<br />
-				<strong className='signup_link'>
-					<Link to='/signup' style={{ color: 'white' }}>
-						{' '}
-						Sign Up
-					</Link>
-				</strong>
-				<span> to make every movie moment unforgettable!</span>
-			</h3>
-		</main>
+				<h3>
+					Explore cinema with FilmFav! <span />
+					Create lists, discover trends, and connect with movie lovers.
+					<br />
+					<strong className='signup_link'>
+						<Link to='/signup' style={{ color: 'white' }}>
+							{' '}
+							Sign Up
+						</Link>
+					</strong>
+					<span> to make every movie moment unforgettable!</span>
+				</h3>
+				{/* <Footer /> */}
+			</main>
+		</>
 	);
 };
 

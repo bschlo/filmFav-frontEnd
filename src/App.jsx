f5adec08990a5b36db2086d445b21cd67571394b
Joemer <joemers97@gmail.com>
2025-01-23 16:45:58 -0500

    added footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 import SignupForm from './components/SignUpForm/SignUpForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
@@ -11,53 +12,51 @@ import MoviesList from './components/MoviesList/MoviesList';
 import MovieDetails from './components/MovieDetails/MovieDetails';
 import * as movieService from './services/movieService';
 
-
 export const AuthedUserContext = createContext(null); // set the initial value of the context to null
 
 const App = () => {
-  const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+	const [user, setUser] = useState(authService.getUser()); // using the method from authservice
 
-  useEffect(() => {
-    const loggedInUser = authService.getUser();
-    if (loggedInUser) {
-      setUser(loggedInUser);
-    }
-  }, []);
+	useEffect(() => {
+		const loggedInUser = authService.getUser();
+		if (loggedInUser) {
+			setUser(loggedInUser);
+		}
+	}, []);
 
-  const handleSignout = () => {
-    authService.signout();
-    setUser(null);
-  };
+	const handleSignout = () => {
+		authService.signout();
+		setUser(null);
+	};
 
-const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const movies = await movieService.index();
-      setMovies(movies);
-    };
-    fetchMovies();
-  }, []);
+	const [movies, setMovies] = useState([]);
+	useEffect(() => {
+		const fetchMovies = async () => {
+			const movies = await movieService.index();
+			setMovies(movies);
+		};
+		fetchMovies();
+	}, []);
 
-
-
-  return (
-    <AuthedUserContext.Provider value={user}>
-      <NavBar handleSignout={handleSignout} />
-      <Routes>
-        {user ? (
-          <>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/movies" element={<MoviesList movies={movies} />} />
-            <Route path="/movies/:movieId" element={<MovieDetails />} />
-          </>
-        ) : (
-          <Route path="/" element={<Landing />} />
-        )}
-        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
-        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
-      </Routes>
-    </AuthedUserContext.Provider>
-  );
+	return (
+		<AuthedUserContext.Provider value={user}>
+			<NavBar handleSignout={handleSignout} />
+			<Routes>
+				{user ? (
+					<>
+						<Route path='/' element={<Dashboard />} />
+						<Route path='/movies' element={<MoviesList movies={movies} />} />
+						<Route path='/movies/:movieId' element={<MovieDetails />} />
+					</>
+				) : (
+					<Route path='/' element={<Landing />} />
+				)}
+				<Route path='/signup' element={<SignupForm setUser={setUser} />} />
+				<Route path='/signin' element={<SigninForm setUser={setUser} />} />
+			</Routes>
+			<Footer />
+		</AuthedUserContext.Provider>
+	);
 };
 
 export default App;

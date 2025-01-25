5423c69a9406dfb8e010336aaa45535660e9b33b
Joemer <joemers97@gmail.com>
2025-01-25 09:35:38 -0500

    enhance layout and design for login form

--- src/components/SigninForm/SigninForm.jsx
+++ src/components/SigninForm/SigninForm.jsx
@@ -1,74 +1,80 @@
-// SigninForm
-
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import * as authService from '../../services/authService'; // import the authservice
+import * as authService from '../../services/authService';
+import './SigninForm.css';
 
 const SigninForm = (props) => {
-  const navigate = useNavigate(); // added this for navigation purposes
-  const [message, setMessage] = useState(['']);
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-  });
+	const navigate = useNavigate();
+	const [message, setMessage] = useState(['']);
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+	});
 
-  const updateMessage = (msg) => {
-    setMessage(msg);
-  };
+	const updateMessage = (msg) => {
+		setMessage(msg);
+	};
 
-  const handleChange = (e) => {
-    updateMessage('');
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+	const handleChange = (e) => {
+		updateMessage('');
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const user = await authService.signin(formData); // TODO build signin service function
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const user = await authService.signin(formData); // TODO build signin service function
 
-      props.setUser(user);
-      navigate('/');
-    } catch (err) {
-      updateMessage(err.message);
-    }
-  };
+			props.setUser(user);
+			navigate('/');
+		} catch (err) {
+			updateMessage(err.message);
+		}
+	};
 
-  return (
-    <main>
-      <h1>Log In</h1>
-      <p>{message}</p>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Username:</label>
-          <input
-            type="text"
-            autoComplete="off"
-            id="username"
-            value={formData.username}
-            name="username"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={formData.password}
-            name="password"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button>Log In</button>
-          <Link to="/">
-            <button>Cancel</button>
-          </Link>
-        </div>
-      </form>
-    </main>
-  );
+	return (
+		<main className='container'>
+			<h1>Log in</h1>
+			<form autoComplete='off' onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor='email'>Username:</label>
+					<input
+						type='text'
+						autoComplete='off'
+						id='username'
+						value={formData.username}
+						name='username'
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<label htmlFor='password'>Password:</label>
+					<input
+						type='password'
+						autoComplete='off'
+						id='password'
+						value={formData.password}
+						name='password'
+						onChange={handleChange}
+					/>
+				</div>
+				<div className='form-sign'>
+					<button>Log In</button>
+					<Link to='/'>
+						<button>Cancel</button>
+					</Link>
+				</div>
+				<p className='signup-link'>
+					Don't have an account? {''}
+					<span>
+						{/* <strong> */}
+						<Link to='/signup'>Sign up here</Link>
+						{/* </strong> */}
+					</span>
+				</p>
+			</form>
+		</main>
+	);
 };
 
 export default SigninForm;

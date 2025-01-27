2ca126d5c8bd4f8d7c2a1aab6407a63ba13fa1e1
bschloss <benschloss2@gmail.com>
2025-01-26 22:06:12 -0500

    Revert "Merge branch 'devbranch' into landing3"
    This reverts commit 28df06ad81d47e333d44f60ea3a3a2a5d4262bd5, reversing
    changes made to 1eb2f5d10bf12071fdbe4de0c973e4dc94cfa804.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,12 +1,11 @@
-const BASE_URL = `${import.meta.env.VITE_BACKEND_SERVER_URL}`;
+const BASE_URL = `${import.meta.env.VITE_BACKEND_SERVER_URL}/movies/`;
 
 const index = async () => {
   try {
-    const res = await fetch(`${BASE_URL}/movies/`, {
+    const res = await fetch(BASE_URL, {
       headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
     });
     return res.json();
-    console.log(res.json())
   } catch (error) {
     console.log(error);
   }
@@ -14,142 +13,10 @@ const index = async () => {
 
 const show = async (movieId) => {
   try {
-    const res = await fetch(`${BASE_URL}/movies/${movieId}/`, {
+    const res = await fetch(`${BASE_URL}${movieId}/`, {
       headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
     })
     return res.json ()
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-
-const showAllReviews = async () => {
-  try {
-    const res = await fetch(`${BASE_URL}/reviews/`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
-    })
-    return res.json ()
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-const createReview = async (movieId, reviewData) => {
-  try {
-    const res = await fetch(`${BASE_URL}/reviews/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('access')}`,
-      },
-      body: JSON.stringify({...reviewData, movie: movieId}),
-    });
-    return res.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const showReviews = async (movieId) => {
-  try {
-    const res = await fetch(`${BASE_URL}/reviews/movie/${movieId}/`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
-    });
-    return res.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const updateReview = async (reviewId, reviewData) => {
-  try {
-    const res = await fetch(`${BASE_URL}/reviews/${reviewId}/`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('access')}`,
-      },
-      body: JSON.stringify(reviewData),
-    });
-    return res.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
-const deleteReview = async (reviewId) => {
-  try {
-    await fetch(`${BASE_URL}/reviews/${reviewId}/`, {
-      method: 'DELETE',
-      headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
-    });
-  } catch (error) {
-    console.error('Error deleting a review:', error);
-  }
-};
-
-const createComment = async (reviewId, commentFormData) => {
-  try {
-    const res = await fetch(`${BASE_URL}/reviews/${reviewId}/comments/`, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('access')}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({...commentFormData, review: reviewId,}),
-    });
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const showComments = async (reviewId) => {
-  try {
-    const res = await fetch(`${BASE_URL}/reviews/${reviewId}/comments/`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
-    });
-    
-    return res.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const deleteComment = async (commentId) => {
-  try {
-    await fetch(`${BASE_URL}/comments/${commentId}/`, {
-      method: 'DELETE',
-      headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
-    });
-  } catch (error) {
-    console.error('Error deleting a comment:', error);
-  }
-};
-const indexPagination = async (url) => {
-  try {
-    const res = await fetch(url, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch movies");
-    }
-    return res.json();
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-};
-
-const showAllMovies = async () => {
-  try {
-    const res = await fetch(`${BASE_URL}/allmovies/`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('access')}` },
-    }); 
-    return res.json()
   } catch (error) {
     console.error(error)
   }
@@ -158,14 +25,4 @@ const showAllMovies = async () => {
 export { 
   index,
   show,
-  createReview,
-  showAllReviews,
-  showReviews,
-  updateReview,
-  deleteReview,
-  createComment,
-  showComments,
-  deleteComment,
-  indexPagination,
-  showAllMovies,
 };
